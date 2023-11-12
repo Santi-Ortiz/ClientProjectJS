3422b054ac486d07a428515cb6a8494a2c132598
Santiago <or.santiago@javeriana.edu.co>
2023-11-12 18:09:12 -0500

    Cambio repositorio

---
+++ src/components/Login.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react"
+
+export default function Login() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    const loginHandle = () => {
+        fetch(`${import.meta.env.VITE_API_URL}/auth/login`, {
+            method : 'POST',
+            headers : {
+                'Authorization' : `Basic ${btoa(`${email}:${password}`)}`
+
+            }
+        }).then(res => res.json())
+        .then(json => {
+            console.log(json)
+
+        }) 
+    }
+
+    return(
+        <>
+        <div style = {{
+            display : 'flex',
+            flexDirection : 'column',
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
+            <input type="email" 
+                value={email} 
+                onChange={e => { setEmail(e.target.value)}} 
+            />
+            <input type="password" 
+                value={password}   
+                onChange={e => { setPassword(e.target.value)}}
+            />
+            <button
+                onClick={loginHandle}
+            >Ingresar</button>
+        </div>
+        
+
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { 
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+import Login from './components/Login.jsx'
+
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <App />
+  },
+  {
+    path: '/element',
+    element: (<><div>Hola, soy un elementico</div></>)
+  },
+  {
+    path : '/login',
+    element : <Login /> 
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
